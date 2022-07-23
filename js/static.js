ed68b176b42ed23a9d22279735918aaf85331653
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-07-23 16:55:06 +0300

    add js elements;)

--- js/static.js
+++ js/static.js
@@ -3,7 +3,7 @@
  let sectionActiveNode = document.querySelectorAll('.section')
  for (let i = 0; i < headerItemlink.length; i++) {
      headerItemlink[i].addEventListener('click', function() {
-         let inde = this.dataset.id
+         let index = this.dataset.id
          for (let t = 0; t < headerItemlink.length; t++) {
              headerItemlink[t].classList.remove('tabs__nav-itemlink--active')
          }
@@ -11,7 +11,7 @@
          for (let b = 0; b < sectionActiveNode.length; b++) {
              sectionActiveNode[b].classList.remove('section--active')
          }
-         sectionActiveNode[inde].classList.add('section--active')
+         sectionActiveNode[index].classList.add('section--active')
      })
  }
  //  tabs__nav-itemlink--active
@@ -19,7 +19,6 @@
 
  // откытие меню фильтра при клике.
  let openDrrop = document.querySelectorAll('.filter__drropdown')
- let drropNode = document.querySelectorAll('.drropdown')
  openDrrop.forEach(function(e) {
      e.addEventListener('click', function() {
          openDrrop.forEach(function(e) {
