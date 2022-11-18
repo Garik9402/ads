94f68b41e3122dfea333f15943cf7674fa0c0db4
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-11-18 15:23:44 +0300

    in the process:)

--- js/static.js
+++ js/static.js
@@ -1,41 +1,55 @@
- //табы
- let headerItemlink = document.querySelectorAll('.tabs__nav-itemlink')
- let sectionActiveNode = document.querySelectorAll('.section')
- for (let i = 0; i < headerItemlink.length; i++) {
-     headerItemlink[i].addEventListener('click', function() {
-         let index = this.dataset.id
-         for (let t = 0; t < headerItemlink.length; t++) {
-             headerItemlink[t].classList.remove('tabs__nav-itemlink--active')
-         }
-         this.classList.add('tabs__nav-itemlink--active')
-         for (let b = 0; b < sectionActiveNode.length; b++) {
-             sectionActiveNode[b].classList.remove('section--active')
-         }
-         sectionActiveNode[index].classList.add('section--active')
-     })
- }
- //  tabs__nav-itemlink--active
+//табы
+let headerItemlink = document.querySelectorAll('.tabs__nav-itemlink')
+let sectionActiveNode = document.querySelectorAll('.section')
+for (let i = 0; i < headerItemlink.length; i++) {
+   headerItemlink[i].addEventListener('click', function () {
+      let index = this.dataset.id
+      for (let t = 0; t < headerItemlink.length; t++) {
+         headerItemlink[t].classList.remove('tabs__nav-itemlink--active')
+      }
+      this.classList.add('tabs__nav-itemlink--active')
+      for (let b = 0; b < sectionActiveNode.length; b++) {
+         sectionActiveNode[b].classList.remove('section--active')
+      }
+      sectionActiveNode[index].classList.add('section--active')
+   })
+}
+//  tabs__nav-itemlink--active
 
 
- // откытие меню фильтра при клике.
- let openDrrop = document.querySelectorAll('.filter__drropdown')
- openDrrop.forEach(function(e) {
-     e.addEventListener('click', function() {
-         openDrrop.forEach(function(e) {
-             e.classList.remove('filter__drropdown--js-toggle-opn-drrop')
-         })
-         this.classList.add('filter__drropdown--js-toggle-opn-drrop')
+// откытие меню фильтра при клике.
+let openDrrop = document.querySelectorAll('.filter__drropdown')
+openDrrop.forEach(function (e) {
+   e.addEventListener('click', function () {
+      openDrrop.forEach(function (e) {
+         e.classList.remove('filter__drropdown--js-toggle-opn-drrop')
+      })
+      this.classList.add('filter__drropdown--js-toggle-opn-drrop')
 
-     })
- })
+   })
+})
 
 
- window.addEventListener('click', function(e) {
-     openDrrop.forEach(function(item) {
-         if (item.contains(e.target)) {
-             item.classList.add('filter__drropdown--js-toggle-opn-drrop')
-         } else {
-             item.classList.remove('filter__drropdown--js-toggle-opn-drrop')
-         }
-     })
- });
+window.addEventListener('click', function (e) {
+   openDrrop.forEach(function (item) {
+      if (item.contains(e.target)) {
+         item.classList.add('filter__drropdown--js-toggle-opn-drrop')
+      } else {
+         item.classList.remove('filter__drropdown--js-toggle-opn-drrop')
+      }
+   })
+});
+
+
+// открытие дропдауна в доске с иконками 
+let drropdownListNode = document.querySelectorAll('.drropdown-btns-board')
+let iconMore = document.querySelectorAll('.icon-more')
+iconMore.forEach(elem => {
+   elem.addEventListener('click', function () {
+      iconMore.forEach(el => {
+         el.classList.remove('icon-more--js-active')
+      })
+      this.classList.add('icon-more--js-active')
+
+   })
+})
