71c10ba8a5ebc2110d67cdbea24258bb26ffa802
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-11-20 20:37:44 +0300

    in the process;)

--- js/static.js
+++ js/static.js
@@ -1,6 +1,6 @@
 //slider 
 $(document).ready(function () {
-   $(".cards__img-slider").slick({
+   $(".card__slider").slick({
       arrows: false,
       dots: true,
       slidesToShow: 1,
@@ -16,6 +16,7 @@ $(document).ready(function () {
 //табы
 let headerItemlink = document.querySelectorAll('.tabs__nav-itemlink')
 let sectionActiveNode = document.querySelectorAll('.section')
+let checkboxNode = document.querySelectorAll('.filter__checkbox')
 for (let i = 0; i < headerItemlink.length; i++) {
    headerItemlink[i].addEventListener('click', function () {
       let index = this.dataset.id
@@ -27,9 +28,24 @@ for (let i = 0; i < headerItemlink.length; i++) {
          sectionActiveNode[b].classList.remove('section--active')
       }
       sectionActiveNode[index].classList.add('section--active')
+      for (let f = 0; f < checkboxNode.length; f++) {
+         checkboxNode[f].classList.remove('filter__checkbox--js-active')
+      }
+      headerItemlink[i].removeAttribute('data.id', 'nul')
+      checkboxNode[index].classList.add('filter__checkbox--js-active')
+
    })
 }
-
+// переход с активных публикаций в архив
+checkboxNode.forEach(el => {
+   el.addEventListener('click', function () {
+      checkboxNode.forEach(elem => {
+         elem.classList.remove('filter__checkbox--js-active')
+      })
+      this.classList.add('filter__checkbox--js-active')
+   })
+})
+//работа с архивом(публикация карты) + чекбоксы
 
 // откытие меню фильтра при клике.
 let openDrrop = document.querySelectorAll('.filter__drropdown')
@@ -56,7 +72,6 @@ window.addEventListener('click', function (e) {
 
 
 // открытие дропдауна в доске с иконками 
-let drropdownListNode = document.querySelectorAll('.drropdown-btns-board')
 let iconMore = document.querySelectorAll('.icon-more')
 iconMore.forEach(elem => {
    elem.addEventListener('click', function () {
@@ -64,15 +79,14 @@ iconMore.forEach(elem => {
          el.classList.remove('icon-more--js-active')
       })
       this.classList.add('icon-more--js-active')
-
    })
 })
-// добавление карт в архив и количество 
+// добавление карты в архив + чекбоксы
 document.querySelector('.filter__checkbox-name-w').addEventListener('click', funcApp)
 function funcApp() {
-   const cardsNode = Array.from(document.querySelectorAll('.cards__items'))
+   const cardsNode = Array.from(document.querySelectorAll('.card'))
    const archiveNode = document.getElementById('archive')
-   const checkboxMain = document.querySelector('.checkbox-main')
+   const checkboxMain = document.querySelector('.checkbox-active')
    checkboxMain.addEventListener('click', function () {
       let arr = Array.from(activedNode)
       arr.forEach(e => {
@@ -90,12 +104,72 @@ function funcApp() {
       if (el.querySelector('.checkbox').checked == true) {
          archiveNode.append(el)
          el.querySelector('.checkbox').checked = false
-         let amoutActivedCards = document.querySelector('.actived-amount')
-         let amoutArchiveCards = document.querySelector('.archive-amount')
-         amoutActivedCards.innerText = activedNode.length
-         amoutArchiveCards.innerText = archiveNode.children.length
+         amountFunc()
       }
 
    })
 }
 funcApp()
+
+// добавление в архив карты при нажатий на соответствующюю кнопку в дорпдауне
+let archiveItems = document.querySelectorAll('.add-archive')
+archiveItems.forEach(el => {
+   el.addEventListener('click', function () {
+      const archiveNode = document.getElementById('archive')
+      let card = el.closest('.card')
+      archiveNode.append(card)
+      amountFunc()
+   })
+
+})
+// количество товаров 'Aктивные' 'Архив'
+function amountFunc() {
+   let activedNode = document.querySelector('.actived').children
+   let amoutActivedCards = document.querySelector('.actived-amount')
+   let amoutArchiveCards = document.querySelector('.archive-amount')
+   const archiveNode = document.getElementById('archive')
+   amoutActivedCards.innerText = activedNode.length
+   amoutArchiveCards.innerText = archiveNode.children.length
+}
+
+// сортировка карты по цене
+const itemInc = document.getElementById('item-increase').addEventListener('click', funcInc)
+const itemDesc = document.getElementById('item-descending').addEventListener('click', funcDesc)
+function funcDesc() {
+   let activedNode = document.querySelector('#actived')
+   let items = activedNode.childNodes;
+   let itemsArr = [];
+   for (let i in items) {
+      if (items[i].nodeType == 1) {
+         itemsArr.push(items[i]);
+      }
+   }
+   itemsArr.sort(function (a, b) {
+      return parseFloat(a.getAttribute('data-price')) == parseFloat(b.getAttribute('data-price')) ?
+         0 :
+         (parseFloat(a.getAttribute('data-price')) > parseFloat(b.getAttribute('data-price')) ? 1 : -1);
+   });
+   for (let i = 0; i < itemsArr.length; ++i) {
+      activedNode.appendChild(itemsArr[i]);
+   }
+}
+
+function funcInc() {
+   let activedNode = document.querySelector('#actived')
+   let items = activedNode.childNodes;
+   let itemsArr = [];
+   for (let i in items) {
+      if (items[i].nodeType == 1) {
+         itemsArr.push(items[i]);
+      }
+   }
+   itemsArr.sort(function (a, b) {
+      return parseFloat(a.getAttribute('data-price')) == parseFloat(b.getAttribute('data-price')) ?
+         0 :
+         (parseFloat(a.getAttribute('data-price')) < parseFloat(b.getAttribute('data-price')) ? 1 : -1);
+   });
+   for (let i = 0; i < itemsArr.length; ++i) {
+      activedNode.appendChild(itemsArr[i]);
+   }
+}
+
